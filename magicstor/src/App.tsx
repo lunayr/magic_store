86bce722ba117095e3f3d610ef6fdf84499dc383
lunaluday <144939306+lunayr@users.noreply.github.com>
2024-11-19 23:30:23 +0100

    unnecessary import removed

--- magicstor/src/App.tsx
+++ magicstor/src/App.tsx
@@ -1,6 +1,4 @@
-import Basket from "./app_components/Basket";
-import ProductCard from "./app_components/ProductCard";
-import Search from "./app_components/Search";
+
 import { MagicStore } from "./context/ShopContext";
 
 function App() {
